69c9c6ae2bec27aec26a1789de996786d091801f
Ali Reza <webdeve1083@gmail.com>
2023-02-17 06:56:35 +0800

    Fixed a bug: index -> Index

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 
 type Props = {};
 
-export default function index() {
+export default function Index() {
   const router = useRouter();
   useEffect(() => {
     router.push("/docs/intro");
