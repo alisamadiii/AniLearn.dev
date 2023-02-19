6a9634de1a3bb242f0e2ac91d61369d8afaae1f8
Ali Reza <webdeve1083@gmail.com>
2023-02-19 10:18:14 +0800

    Added @vercel/analytics for the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import Layouts from "@/layouts";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layouts>
         <Component {...pageProps} />
+        <Analytics />
       </Layouts>
     </>
   );
