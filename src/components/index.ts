80c2577ef99d9051a2e370f5e0adb1fbc0cf1e55
Ali Reza <webdeve1083@gmail.com>
2023-05-04 14:39:41 +0800

    Deleted all my Previous files and folders

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Buttons } from "./Buttons";
-export { default as Product } from "./Product";
-export { default as Price } from "./Price";
-export { default as Navbar } from "./Navbar";
