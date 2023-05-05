126b9f7f54c311bc524c5fcc2e897d19363b6195
Ali Reza <webdeve1083@gmail.com>
2023-05-05 13:34:37 +0800

    Done with justify-content & align-items section

---
+++ src/components/Button_Tech.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type Props = {
+  value: string;
+  classNameValue: string;
+  className: string;
+  setClassName: (a: string) => void;
+};
+
+export default function Button_Tech({
+  value,
+  classNameValue,
+  className,
+  setClassName,
+}: Props) {
+  return (
+    <button
+      onClick={() => setClassName(classNameValue)}
+      className={`relative px-4 py-2 rounded-lg text-white bg-button after:content after:w-full after:h-full after:bg-gradient-to-r after:from-primary after:to-secondary after:absolute after:inset-0 after:-z-10 after:opacity-0 after:duration-200 overflow-hidden duration-200 isolate ${
+        className == classNameValue
+          ? "after:opacity-100 shadow-main-button"
+          : "after:opacity-0"
+      }`}
+    >
+      {value}
+    </button>
+  );
+}
--- src/pages/css/flex-position.tsx
+++ src/pages/css/flex-position.tsx
@@ -1,10 +1,16 @@
 import { Heading_1, Heading_2, Heading_3 } from "@components/Headings";
 import Container from "@layouts/Container";
 import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {};
 
+// Icons
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import Button_Tech from "@components/Button_Tech";
+
 export default function Flex_Position({}: Props) {
+  const [rectsNum, setRectsNum] = useState(3);
   // First State Management
   const [justifyContent, setJustifyContent] = useState("justify-start");
   const [alignItems, setAlignItems] = useState("items-start");
@@ -15,6 +21,16 @@ export default function Flex_Position({}: Props) {
   // Third State Management
   const [justifySelf, setJustifySelf] = useState("justify-self-start");
   const [alignSelf, setAlignSelf] = useState("self-start");
+
+  const rectsFunction = (value: "increase" | "decrease") => {
+    if (value == "increase") {
+      setRectsNum(rectsNum + 1);
+      if (rectsNum >= 10) setRectsNum(10); // max=10
+    } else {
+      setRectsNum(rectsNum - 1);
+      if (rectsNum <= 3) setRectsNum(3); // min=3
+    }
+  };
 
   return (
     <Container className="py-12">
@@ -42,9 +58,121 @@ export default function Flex_Position({}: Props) {
         This is the place where you can try them.
       </p>
 
+      {/* Counts */}
+      <div
+        className="flex items-center justify-between p-4 my-8 rounded-lg"
+        style={{
+          background: `rgba(18, 20, 24, ${
+            rectsNum == 10 ? "1" : `.${rectsNum}`
+          })`,
+        }}
+      >
+        <motion.span
+          key={rectsNum}
+          initial={{ y: -5, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className="text-xl"
+        >
+          {rectsNum}
+        </motion.span>
+        <div className="flex flex-col">
+          <button
+            value="increase"
+            onClick={(e: any) => rectsFunction(e.target.value)}
+          >
+            <IoIosArrowUp className="pointer-events-none" />
+          </button>
+          <button
+            value="decrease"
+            onClick={(e: any) => rectsFunction(e.target.value)}
+          >
+            <IoIosArrowDown className="pointer-events-none" />
+          </button>
+        </div>
+      </div>
+
       <Heading_3 className="mt-8 text-3xl">
         justify-content & align-items
       </Heading_3>
+      {/* Workplace */}
+      <div className="mt-8">
+        {/* Demo */}
+        <div
+          className={`w-full max-w-[905px] mx-auto h-36 bg-box border border-white-low-opacity rounded-lg flex gap-1 overflow-hidden ${justifyContent} ${alignItems}`}
+        >
+          <AnimatePresence initial={false}>
+            {[...Array(rectsNum)].map((_, index) => (
+              <motion.div
+                key={index}
+                initial={{ y: 10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                layout
+                className="bg-white rounded-lg w-11 h-11"
+              />
+            ))}
+          </AnimatePresence>
+        </div>
+        {/* Buttons */}
+        <div className="flex flex-wrap justify-center gap-4 mt-8">
+          <Button_Tech
+            value="start"
+            classNameValue="justify-start"
+            className={justifyContent}
+            setClassName={setJustifyContent}
+          />
+          <Button_Tech
+            value="end"
+            classNameValue="justify-end"
+            className={justifyContent}
+            setClassName={setJustifyContent}
+          />
+          <Button_Tech
+            value="center"
+            classNameValue="justify-center"
+            className={justifyContent}
+            setClassName={setJustifyContent}
+          />
+          <Button_Tech
+            value="space-between"
+            classNameValue="justify-between"
+            className={justifyContent}
+            setClassName={setJustifyContent}
+          />
+          <Button_Tech
+            value="space-evenly"
+            classNameValue="justify-evenly"
+            className={justifyContent}
+            setClassName={setJustifyContent}
+          />
+          <Button_Tech
+            value="space-around"
+            classNameValue="justify-around"
+            className={justifyContent}
+            setClassName={setJustifyContent}
+          />
+        </div>
+        <div className="flex flex-wrap justify-center gap-4 mt-4">
+          <Button_Tech
+            value="start"
+            classNameValue="items-start"
+            className={alignItems}
+            setClassName={setAlignItems}
+          />
+          <Button_Tech
+            value="end"
+            classNameValue="items-end"
+            className={alignItems}
+            setClassName={setAlignItems}
+          />
+          <Button_Tech
+            value="center"
+            classNameValue="items-center"
+            className={alignItems}
+            setClassName={setAlignItems}
+          />
+        </div>
+      </div>
 
       <Heading_3 className="mt-8 text-3xl">align-content</Heading_3>
 
