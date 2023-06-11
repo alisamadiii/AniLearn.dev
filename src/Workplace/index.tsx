9a97a2111a32080c5419728f80f5374284e730c0
Ali Reza <webdeve1083@gmail.com>
2023-06-11 15:16:21 +0800

    Done with one page (input page), Deleted the docs page

--- src/Workplace/index.tsx
+++ src/Workplace/index.tsx
@@ -9,7 +9,15 @@ type Props = {
 // ===== Workplace =====
 
 export default function Workplace({ children, className }: Props) {
-  return <div className={twMerge("relative mt-12", className)}>{children}</div>;
+  return (
+    <div className="mt-4 mb-12">
+      <h1 className="relative pb-2 mt-8 text-3xl font-semibold text-white capitalize border-b-2 border-white-low-opacity">
+        Working Place
+      </h1>
+      <p className="mb-8">This is the place where you can try them.</p>
+      <div className={twMerge("relative", className)}>{children}</div>
+    </div>
+  );
 }
 
 export function LiveChanges({ children, className }: Props) {
---
+++ src/pages/html/open-graph.tsx
@@ -0,0 +1,8 @@
+import Container from "@layouts/Container";
+import React from "react";
+
+type Props = {};
+
+export default function OpenGraph({}: Props) {
+  return <Container>OpenGraph</Container>;
+}
